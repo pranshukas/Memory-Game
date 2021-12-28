dd866c4165a534aa5df28833f347e59e5119e416
Pranshu Kashyap <pranshu.kashyap.mec19@itbhu.ac.in>
2021-12-29 03:32:33 +0530

    Add ShuffleCards Handler

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
+const cardImages = [
+    { src: "/images/helmet-1.png" },
+    { src: "/images/potion-1.png" },
+    { src: "/images/ring-1.png" },
+    { src: "/images/scroll-1.png" },
+    { src: "/images/shield-1.png" },
+    { src: "/images/sword-1.png" },
+];
+
 function App() {
-    return <div className="App">
-        <h1>Magic Match</h1>
-        <button>New Game</button>
-    </div>;
+    const [cards, setCards] = useState([]);
+    const [turns, setTurns] = useState(0);
+
+    const shuffleCards = () => {
+        const shuffledCards = [...cardImages, ...cardImages].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }));
+
+        setCards(shuffledCards);
+        setTurns(0);
+    };
+
+    console.log(cards, turns);
+
+    return (
+        <div className="App">
+            <h1>Magic Match</h1>
+            <button onClick={shuffleCards}>New Game</button>
+        </div>
+    );
 }
 
 export default App;
